9c94af5a6c9a21f77afde754c45afa36c2044d03
joanieluna <joanie.l@orange.fr>
2020-12-01 16:51:19 +0100

    ajout standard test

--- app/tests/test.js
+++ app/tests/test.js
@@ -1,7 +1,19 @@
-var Bill = require('../src/functions');
+/* eslint-env jest */
 
-test('calcul bill with 0 0',() =>{expect(Bill.calcul_bill([0,0],[0,0])).toBe(0)})
-test('calcul bill with different sizes',() =>{expect(Bill.calcul_bill([0],[0,0])).toBe(-1)})
-test('calcul bill with negative values for prices',() =>{expect(Bill.calcul_bill([-10,5],[10,2])).toBe(10)})
-test('calcul bill with negative values for quantities',() =>{expect(Bill.calcul_bill([10,5],[-10,2])).toBe(10)})
-test('calcul bill with normal values',() =>{expect(Bill.calcul_bill([10,5],[10,2])).toBe(110)})
+const Bill = require('../src/functions')
+
+test('calcul bill with 0 0', () => {
+  expect(Bill.calculBill([0, 0], [0, 0])).toBe(0)
+})
+test('calcul bill with different sizes', () => {
+  expect(Bill.calculBill([0], [0, 0])).toBe(-1)
+})
+test('calcul bill with negative values for prices', () => {
+  expect(Bill.calculBill([-10, 5], [10, 2])).toBe(10)
+})
+test('calcul bill with negative values for quantities', () => {
+  expect(Bill.calculBill([10, 5], [-10, 2])).toBe(10)
+})
+test('calcul bill with normal values', () => {
+  expect(Bill.calculBill([10, 5], [10, 2])).toBe(110)
+})
