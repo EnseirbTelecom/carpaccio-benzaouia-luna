f8b6c0c2eb15c3dfb0ab256a056292b52c588745
dbenzaouia <43379506+dbenzaouia@users.noreply.github.com>
2020-12-01 16:36:37 +0100

    test standard

--- app/src/index.js
+++ app/src/index.js
@@ -1,35 +1,27 @@
 const express = require('express')
 const app = express()
 const id = require('../data/id.json')
-const json = require('express')
-const bodyParser = require('body-parser');
-//const { calcul_bill } = require('./functions');
-var Bill = require('./functions');
+const Bill = require('./functions')
 
+app.use(express.json())
 
-app.use(express.json());
-
-app.get('/id', (req,res) => {
-    res.status(200).json(id)
+app.get('/id', (req, res) => {
+  res.status(200).json(id)
 })
 
-app.post('/bill', (req,res) => {
-    prices = req.body.prices;
-    quantities = req.body.quantities;
-    bills = new Bill 
-    bill = bills.calcul_bill(prices, quantities);
-    if (bill == -1 ){
-        res.status(400).json({error : 'error message'})
-    }
-    else {
-        res.status(200).json({total: bill})
-    }
-    
+app.post('/bill', (req, res) => {
+  const prices = req.body.prices
+  const quantities = req.body.quantities
+  const bills = new Bill()
+  const bill = bills.calcul_bill(prices, quantities)
+  if (bill === -1) {
+    res.status(400).json({ error: 'error message' })
+  }
+  else {
+    res.status(200).json({ total: bill })
+  }    
 })
-
-
 
 app.listen(8080, () => {
-    console.log(`Server listening`)
-  })
-
+  console.log('Server listening')
+})
